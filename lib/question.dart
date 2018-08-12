05987b6861fb5447c26c069995218f2b13070d87
esplo <esplo@users.noreply.github.com>
2018-08-12 22:19:12 +1000

    [modify] change font weight for questions

--- lib/question.dart
+++ lib/question.dart
@@ -11,9 +11,22 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-      Text('${this.words.word1}'),
-      Text('${this.words.word2}'),
-    ]);
+    final textStyle = TextStyle(
+      fontWeight: FontWeight.bold,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Text(
+          '${this.words.word1}',
+          style: textStyle,
+        ),
+        Text(
+          '${this.words.word2}',
+          style: textStyle,
+        ),
+      ],
+    );
   }
 }
