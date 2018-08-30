43b54e9295405822440e2f13f803ca2b90159152
esplo <esplo@users.noreply.github.com>
2018-08-30 23:03:55 +1000

    [refactor] remove unused code

--- lib/remote_score_manager.dart
+++ lib/remote_score_manager.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 typedef void FetchHistoryCallback(QuerySnapshot data);
 
@@ -13,7 +12,6 @@ class RemoteScoreManager {
 
   String uid;
 
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
   static final FirebaseAuth _auth = FirebaseAuth.instance;
 
   RemoteScoreManager._internal(this.uid);
@@ -23,26 +21,6 @@ class RemoteScoreManager {
     assert(user != null);
     final FirebaseUser currentUser = await _auth.currentUser();
     return RemoteScoreManager._internal(currentUser.uid);
-  }
-
-  // TODO: create button
-  Future<String> _testSignInWithGoogle() async {
-    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-    final FirebaseUser user = await _auth.signInWithGoogle(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    assert(user.email != null);
-    assert(user.displayName != null);
-    assert(!user.isAnonymous);
-    assert(await user.getIdToken() != null);
-
-    final FirebaseUser currentUser = await _auth.currentUser();
-    assert(user.uid == currentUser.uid);
-
-    return 'signInWithGoogle succeeded: $user';
   }
 
   void fetchHistory(FetchHistoryCallback callback) {
