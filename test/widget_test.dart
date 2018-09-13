97f69a0456056168d7a0cc97c94585063a4ea718
esplo <esplo@users.noreply.github.com>
2018-09-13 11:54:09 +1000

    [fix] broken test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -26,12 +26,11 @@ void main() {
 
     // Verify that the session has started.
     expect(find.text('START'), findsNothing);
-//    expect(find.text('SCORE: '), findsOneWidget);
-//    expect(find.text('0'), findsOneWidget);
     expect(find.text('30'), findsOneWidget); // TODO: timer
     expect(find.text('same'), findsOneWidget);
     expect(find.text('different'), findsOneWidget);
-    expect(find.text('XEJZ'), findsNWidgets(2));
+    expect(find.text('ZIH'), findsOneWidget);
+    expect(find.text('ZFH'), findsOneWidget);
 
     // Tap the correct answer.
     await tester.tap(find.text('same'));
@@ -39,25 +38,14 @@ void main() {
 
     // correct answer, score increase by 4
     expect(find.text('START'), findsNothing);
-//    expect(find.text('4'), findsOneWidget);
-    expect(find.text('ZIH'), findsOneWidget);
-    expect(find.text('ZFH'), findsOneWidget);
+    expect(find.text('DKSK'), findsOneWidget);
+    expect(find.text('DSSK'), findsOneWidget);
 
     // Tap the correct answer
     await tester.tap(find.text('different'));
     await tester.pump();
 
     // correct answer, score increase by 5
-//    expect(find.text('9'), findsOneWidget);
-    expect(find.text('DKSK'), findsOneWidget);
-    expect(find.text('DSSK'), findsOneWidget);
-
-    // Tap the wrong answer
-    await tester.tap(find.text('same'));
-    await tester.pump();
-
-    // wrong answer, score decrease by 10
     expect(find.text('SUJIZAVY'), findsNWidgets(2));
-//    expect(find.text('-1'), findsOneWidget);
   });
 }
